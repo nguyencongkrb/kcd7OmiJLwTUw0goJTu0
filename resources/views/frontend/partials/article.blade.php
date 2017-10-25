da2b9ff57ea993362d84779d7a0f01359de873bb
phantsang <phantsang@users.noreply.github.com>
2017-10-25 13:37:49 +0700

    off image article detail

--- resources/views/frontend/partials/article.blade.php
+++ resources/views/frontend/partials/article.blade.php
@@ -2,12 +2,6 @@
 <br><br>
 @endif
 <h1 class="article-title">{{ $article->name }}</h1>
-<div class="text-center">
-	<center>
-	<img src="{{ $article->getFirstAttachment('custom', 600, 400) }}" class="img-responsive">
-	</center>
-</div>
-<br>
 {!! $article->content !!}
 @if(Auth::guest())
 <br><br>
